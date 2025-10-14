11e9142691bddd34866c855cbc783df199345293
christmas <pixedit.2023@gmail.com>
2025-10-14 11:28:48 +0330

    Change the layout and background

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ const minuteHand = document.querySelector(".minute");
 const secondHand = document.querySelector(".second");
 
 ////////////// POSITIONING THE CHARACTERS OF CLOCK ON A CIRCLE
-for (let i = 1; i <= 12; i++) {
-	let angle = (i + 9) * 30;
-	// +9 makes the characters stay at the right position
-	// i * 30 because 360 degree of a circle / 12 = 30 degree
+// for (let i = 1; i <= 12; i++) {
+// 	let angle = (i + 9) * 30;
+// 	// +9 makes the characters stay at the right position
+// 	// i * 30 because 360 degree of a circle / 12 = 30 degree
 
-	const element = document.createElement("div");
-	element.textContent = i;
-	element.classList.add("hour-character");
-	clock.appendChild(element);
+// 	const element = document.createElement("div");
+// 	element.textContent = i;
+// 	element.classList.add("hour-character");
+// 	clock.appendChild(element);
 
-	element.style.setProperty("--angle", `${angle}deg`);
-}
+// 	element.style.setProperty("--angle", `${angle}deg`);
+// }
 
 //////////////////////// UPDATING THE CLOCK HANDS EVERY SECOND
 function updateClock() {
