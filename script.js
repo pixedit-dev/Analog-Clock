cb3871237121094b15069b6d93afa43ae9cffa4d
christmas <pixedit.2023@gmail.com>
2025-10-14 07:46:05 +0330

    Add update function per second

--- script.js
+++ script.js
@@ -16,3 +16,24 @@ for (let i = 1; i <= 12; i++) {
 
 	element.style.setProperty("--angle", `${angle}deg`);
 }
+
+//////////////////////// UPDATING THE CLOCK HANDS EVERY SECOND
+function updateClock() {
+	const now = new Date();
+	const hours = now.getHours() % 12; // 12 hours
+	const minutes = now.getMinutes();
+	const seconds = now.getSeconds();
+
+	const hourDeg = hours * 30 + minutes * 0.5;
+	// 360deg clock / 12 hours = 30deg. we need to calc minute effect on hour degree
+	// 30 / 60 minutes = 0.5deg | hourDeg = hours * 30 + minutes * 0.5
+	const minuteDeg = minutes * 6; // 360deg / 60 minutes = 6deg
+	const secondDeg = seconds * 6;
+
+	hourHand.style.transform = `rotate(${hourDeg}deg)`;
+	minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
+	secondHand.style.transform = `rotate(${secondDeg}deg)`;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
